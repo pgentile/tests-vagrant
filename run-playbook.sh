e018eeac6813e975d82054a5cc57cd70d11db75d
Pierre Gentile <pierre.gentile.perso@gmail.com>
2015-08-19 20:11:21 +0200

    Simplification commande

--- run-playbook.sh
+++ run-playbook.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-exec ansible-playbook -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory $@
+args=$@
+if [[ -z "$args" ]]
+then
+    args="--skip-tags=fix"
+fi
+
+exec ansible-playbook -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory provisioning/site.yml $args
