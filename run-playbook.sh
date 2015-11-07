d1d661941f177b6013cd88c332d812447f1fec40
Pierre Gentile <pierre.gentile.perso@gmail.com>
2015-11-07 12:53:46 +0100

    Suppression des blocs 'fix'

--- run-playbook.sh
+++ run-playbook.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 args=$@
-if [[ -z "$args" ]]
-then
-    args="--skip-tags=fix"
-fi
+# if [[ -z "$args" ]]
+# then
+#     args="--skip-tags=fix"
+# fi
 
 exec ansible-playbook \
     -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory \
